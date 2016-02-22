12eae1db0b4c0ebcdb3090dc04abbc4a19ae02c6
Joseph C. Slater <joseph.c.slater@gmail.com>
2016-02-22 16:02:12 -0500

    Update no longer gives warning if toolbox isn't installed.

--- inst/vtbud.m
+++ inst/vtbud.m
@@ -1,4 +1,11 @@
-pkg uninstall vtoolbox
+try
+    pkg load vtoolbox
+end
+
+if exist('vtb1_1.m') == 2
+    pkg uninstall vtoolbox
+end
+
 [s, success, message]=urlwrite('https://github.com/josephcslater/ovtoolbox/archive/master.zip','vtoolbox.zip')
 pkg install vtoolbox.zip
 delete('vtoolbox.zip')
