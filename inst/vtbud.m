f67a8698fc1e6c974ba95b8b10a18de2c091af30
Joseph C. Slater <joseph.c.slater@gmail.com>
2016-02-29 17:51:22 -0500

    wrong repository

--- inst/vtbud.m
+++ inst/vtbud.m
@@ -6,7 +6,7 @@ if exist('vtb1_1.m') == 2
     pkg uninstall vtoolbox
 end
 
-[s, success, message]=urlwrite('https://github.com/josephcslater/ovtoolbox/archive/master.zip','vtoolbox.zip')
+[s, success, message]=urlwrite('https://github.com/vibrationtoolbox/ovtoolbox/archive/master.zip','vtoolbox.zip')
 pkg install vtoolbox.zip
 delete('vtoolbox.zip')
 ;
